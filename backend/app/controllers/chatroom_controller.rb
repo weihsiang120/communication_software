1ea747074557448702131fba7a53f57cdc9f38d9
yoyoNTNU <yoyo102102102@gmail.com>
2023-07-06 11:57:22 +0800

    feat:call all messages record

--- backend/app/controllers/chatroom_controller.rb
+++ backend/app/controllers/chatroom_controller.rb
@@ -1,7 +1,9 @@
 class ChatroomController < ApplicationController
   def index
+    @messages=Message.all
   end
   def speak
 		ActionCable.server.broadcast('chat_channel', params[:speak_content])
+    @message=Message.create(type_:"string",chatroom_id:1,member_id:1,content:params[:speak_content])
 	end
 end
